564bf3881ddd08490f6f48da399e92134709def2
Volodymyr Bilohorodskyi <niko.fm@gmail.com>
2024-05-21 14:57:53 +0300

    Homework 7 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,13 @@
+"use strict";
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
 }
+function changeBgColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+    const span = document.querySelector(".color");
+    span.innerText = getRandomHexColor();
+}
+const button = document.querySelector(".change-color");
+button.addEventListener("click", changeBgColor);
